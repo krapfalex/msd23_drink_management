9a77bd9e816907d31a44acfaa3bb12944355cb97
Patrick Riedler <patrick.riedler@edu.fh-joanneum.at>
2024-07-04 11:47:05 +0200

    Implement Cocktail methods and documentation

--- src/main/java/at/fhj/msd/Cocktail.java
+++ src/main/java/at/fhj/msd/Cocktail.java
@@ -13,28 +13,66 @@ public class Cocktail implements Drink{
         this.liquids = liquids;
     }
 
+    /**
+     * Calculates and returns volume of drink
+     *
+     * @return the volume of drink in liter
+     */
     @Override
     public double getVolume() {
-        return 0;
+        double volume = 0;
+        for (Liquid liquid: liquids) {
+            volume += liquid.getVolume();
+        }
+        return volume;
     }
 
+    /**
+     * Calculates and returns the alcohol percentage
+     *
+     * @return alcohol volume percent (e.g. 50)
+     */
     @Override
     public double getAlcoholPercent() {
-        return 0;
+        double percentage = 0;
+        for (Liquid liquid: liquids) {
+            percentage += liquid.getAlcoholPercent();
+        }
+        return percentage;
     }
 
+    /**
+     * Gives information if drink is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
     @Override
     public boolean isAlcoholic() {
+        for (Liquid liquid: liquids) {
+            if (liquid.getAlcoholPercent() > 0) {
+                return true;
+            }
+        }
         return false;
     }
 
+    /**
+     * Gets the name of the drink
+     *
+     * @return the name of the drink
+     */
     @Override
     public String getName() {
-        return null;
+        return cocktailName;
     }
 
+    /**
+     * Sets the name of the drink
+     *
+     * @param name the name of the drink
+     */
     @Override
     public void setName(String name) {
-
+        this.cocktailName = name;
     }
 }
